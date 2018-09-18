9ac5a2d7b1cc3e411a9f0095ca2260fd738f1ab8
Vincent Li <vli8558@bths.edu>
2018-09-18 10:14:30 -0400

    End of class update 9/18
    WIP leastCommonMultiple()

--- src/VLi/TestFile.java
+++ src/VLi/TestFile.java
@@ -1,12 +1,27 @@
 package VLi;
 
-import static VLi.VLiLib.println;
+import static VLi.VLiLib.*;
 
 public class TestFile
 {
     public static void main(String[] args)
     {
-        println("You are my little apple, little apple.");
+        //There is no b inside catatonic cat and as such the line that gets printed is just catatonic cat
+        println(cutOut("catatonic cat","b"));
 
+        //While there is multiple cat inside catatonic cat, only the first one is removed leaving atonic cat
+        println(cutOut("catatonic cat","cat"));
+
+        //This is simple enough, if the string is entered in the format of 04/20/2001 then it returns it as 20/04/2001
+        println(dateStr("04/20/2001"));
+
+        //This prints 5 lines, the first 4 multiples of 4 (except 0)
+        multiplicationTable(4,4);
+
+        //The method adds all numbers from 0 to 10 and results in 55.
+        println(sumUpTo(10));
+
+        //
+        println(leastCommonMultiple(2,4,6));
     }
 }
--- src/VLi/VLiLib.java
+++ src/VLi/VLiLib.java
@@ -6,8 +6,48 @@ public class VLiLib
     {
         System.out.println(str);
     }
-    public static void cutOut(String mainStr, String subStr)
+    public static String cutOut(String mainStr, String subStr)
+    {
+        int check = mainStr.indexOf(subStr);
+        if(check > -1)
+        {
+            mainStr = mainStr.replaceFirst(subStr,"");
+        }
+        return(mainStr);
+        // using the replaceFirst function I just replaced whatever the subStr is with an empty string
+    }
+    public static String dateStr(String date)
+    {
+        String month = date.substring(0,date.indexOf("/"));
+        String day = date.substring(date.indexOf("/")+1,5);
+        String year = date.substring(6,date.length());
+        date = day + "-"+ month + "-" + year;
+        return(date);
+        //I break the inserted string into 3 smaller strings, one for day,month and year, then reassemble them in the provided format.
+    }
+    public static void multiplicationTable(int base, int range)
+    {
+        for(int i=0;i<=range;i++)
+        {
+            String num = String.valueOf(base * i);
+            println(num);
+            //It just prints out the multiples of base based off of range
+        }
+    }
+    public static String sumUpTo(int upTo)
+    {
+        int sum = 0;
+        for(int i = 0; i <= upTo;i++)
+        {
+            sum += i;
+            //The for loop uses the parameter to dictate how many numbers to add to the total sum
+        }
+        String str = String.valueOf(sum);
+        return (str);
+    }
+    public static String leastCommonMultiple(int num1, int num2, int num3)
     {
 
+        return(String.valueOf(num1*num2*num3));
     }
 }
