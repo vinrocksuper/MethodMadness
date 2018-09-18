48c91fe8505fd5bd90b239196732704186162be2
BT_1N3_18@bktech.local <BT_1N3_18@bktech.local>
2018-09-18 09:16:02 -0400

    Initial commit

---
+++ src/VLi/TestFile.java
@@ -0,0 +1,12 @@
+package VLi;
+
+import static VLi.VLiLib.println;
+
+public class TestFile
+{
+    public static void main(String[] args)
+    {
+        println("You are my little apple, little apple.");
+
+    }
+}
---
+++ src/VLi/VLiLib.java
@@ -0,0 +1,13 @@
+package VLi;
+
+public class VLiLib
+{
+    public static void println(String str)
+    {
+        System.out.println(str);
+    }
+    public static void cutOut(String mainStr, String subStr)
+    {
+
+    }
+}
